bdd8020086f19a7f9317d4a107502bca6b82f759
Onesmus <kinaroonesmus7@gmail.com>
2026-07-06 13:01:54 +0300

    Fix Drift annotation import for build_runner

--- lib/core/database/database.dart
+++ lib/core/database/database.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:drift/dart.dart';
+import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
